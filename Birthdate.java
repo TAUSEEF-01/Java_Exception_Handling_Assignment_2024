b839085900b474fe1ff90a250e07c33c468ef03e
TAUSEEF-01 <thesupremeemperor01@gmail.com>
2024-04-20 21:40:59 +0600

    files updated

--- Birthdate.java
+++ Birthdate.java
@@ -494,3 +494,329 @@ public class Birthdate {
         System.out.flush();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Birthdate_Exception extends Exception {
+//     public Birthdate_Exception(String s) {
+//         super(s);
+//     }
+// }
+
+// public class Birthdate {
+
+//     String bd;
+
+//     public void set_birthdate() {
+//         Scanner sc = new Scanner(System.in);
+
+//         try {
+//             System.out.println("Enter Birth date: (Date-Month-Year)");
+//             bd = sc.nextLine();
+
+//             if (bd.isEmpty())
+//                 throw new Birthdate_Exception("Birth date Exception found!");
+
+//             int len = bd.length();
+//             int p1 = len, p2 = len;
+
+//             try {
+//                 boolean f = false;
+
+//                 for (int i = 0; i < len; i++) {
+//                     if (bd.charAt(i) == ' ') {
+//                         p1 = i + 1;
+//                         break;
+//                     }
+//                     if (!(bd.charAt(i) >= '0' && bd.charAt(i) <= '9')) {
+//                         f = true;
+//                         break;
+//                     }
+//                 }
+
+//                 if (f) {
+//                     throw new Birthdate_Exception("Birth date Exception found!");
+//                 }
+//             } catch (Birthdate_Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             } catch (Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             }
+
+//             try {
+//                 boolean f = false;
+
+//                 for (int i = p1; i < len; i++) {
+//                     if (bd.charAt(i) == ' ') {
+//                         p2 = i + 1;
+//                         break;
+//                     }
+//                     if (!((bd.charAt(i) >= 'A' && bd.charAt(i) <= 'Z')
+//                             || (bd.charAt(i) >= 'a' && bd.charAt(i) <= 'z'))) {
+//                         f = true;
+//                         break;
+//                     }
+//                 }
+
+//                 if (f) {
+//                     throw new Birthdate_Exception("Birth date Exception found!");
+//                 }
+//             } catch (Birthdate_Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             } catch (Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             }
+
+//             try {
+//                 boolean f = false;
+
+//                 for (int i = p2; i < len; i++) {
+//                     if (!(bd.charAt(i) >= '0' && bd.charAt(i) <= '9')) {
+//                         f = true;
+//                         break;
+//                     }
+//                 }
+
+//                 if (f || ((len - p2) != 4)) {
+//                     throw new Birthdate_Exception("Birth date Exception found!");
+//                 }
+//             } catch (Birthdate_Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             } catch (Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             }
+
+//             try {
+//                 if (p1 == len || p2 == len) {
+//                     throw new Birthdate_Exception("Birth date Exception found!");
+//                 }
+//             } catch (Birthdate_Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             } catch (Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             }
+
+//         } catch (Birthdate_Exception e) {
+//             bd = "";
+//             System.out.println("BirthdateBlank " + e);
+//         } catch (Exception e) {
+//             bd = "";
+//             System.out.println("BirthdateBlank " + e);
+//         } finally {
+//             System.out.println();
+//             System.out.println("Birth date page ends here!");
+//         }
+
+//         try {
+//             Thread.sleep(1000);
+//         } catch (InterruptedException e) {
+//             e.printStackTrace();
+//         }
+
+//         clearScreen();
+//     }
+
+//     public String get_birthdate() {
+
+//         try {
+
+//             if (bd.isEmpty())
+//                 throw new Birthdate_Exception("Birth date Exception found!");
+
+//             int len = bd.length();
+//             int p1 = len, p2 = len;
+
+//             try {
+//                 boolean f = false;
+
+//                 for (int i = 0; i < len; i++) {
+//                     if (bd.charAt(i) == ' ') {
+//                         p1 = i + 1;
+//                         break;
+//                     }
+//                     if (!(bd.charAt(i) >= '0' && bd.charAt(i) <= '9')) {
+//                         f = true;
+//                         break;
+//                     }
+//                 }
+
+//                 if (f) {
+//                     throw new Birthdate_Exception("Birth date Exception found!");
+//                 }
+//             } catch (Birthdate_Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             } catch (Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             }
+
+//             try {
+//                 boolean f = false;
+
+//                 for (int i = p1; i < len; i++) {
+//                     if (bd.charAt(i) == ' ') {
+//                         p2 = i + 1;
+//                         break;
+//                     }
+//                     if (!((bd.charAt(i) >= 'A' && bd.charAt(i) <= 'Z')
+//                             || (bd.charAt(i) >= 'a' && bd.charAt(i) <= 'z'))) {
+//                         f = true;
+//                         break;
+//                     }
+//                 }
+
+//                 if (f) {
+//                     throw new Birthdate_Exception("Birth date Exception found!");
+//                 }
+//             } catch (Birthdate_Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             } catch (Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             }
+
+//             try {
+//                 boolean f = false;
+
+//                 for (int i = p2; i < len; i++) {
+//                     if (!(bd.charAt(i) >= '0' && bd.charAt(i) <= '9')) {
+//                         f = true;
+//                         break;
+//                     }
+//                 }
+
+//                 if (f || ((len - p2) != 4)) {
+//                     throw new Birthdate_Exception("Birth date Exception found!");
+//                 }
+//             } catch (Birthdate_Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             } catch (Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             }
+
+//             try {
+//                 if (p1 == len || p2 == len) {
+//                     throw new Birthdate_Exception("Birth date Exception found!");
+//                 }
+//             } catch (Birthdate_Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             } catch (Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             }
+//             return bd;
+
+//         } catch (Birthdate_Exception e) {
+//             System.out.println("BirthdateBlank " + e);
+//             return "";
+//         } catch (Exception e) {
+//             System.out.println("BirthdateBlank " + e);
+//             return "";
+//         }
+//     }
+
+//     public void get_birthdate_exceptions() {
+
+//         try {
+
+//             if (bd.isEmpty())
+//                 throw new Birthdate_Exception("Birth date Exception found!");
+
+//             int len = bd.length();
+//             int p1 = len, p2 = len;
+
+//             try {
+//                 boolean f = false;
+
+//                 for (int i = 0; i < len; i++) {
+//                     if (bd.charAt(i) == ' ') {
+//                         p1 = i + 1;
+//                         break;
+//                     }
+//                     if (!(bd.charAt(i) >= '0' && bd.charAt(i) <= '9')) {
+//                         f = true;
+//                         break;
+//                     }
+//                 }
+
+//                 if (f) {
+//                     throw new Birthdate_Exception("Birth date Exception found!");
+//                 }
+//             } catch (Birthdate_Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             } catch (Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             }
+
+//             try {
+//                 boolean f = false;
+
+//                 for (int i = p1; i < len; i++) {
+//                     if (bd.charAt(i) == ' ') {
+//                         p2 = i + 1;
+//                         break;
+//                     }
+//                     if (!((bd.charAt(i) >= 'A' && bd.charAt(i) <= 'Z')
+//                             || (bd.charAt(i) >= 'a' && bd.charAt(i) <= 'z'))) {
+//                         f = true;
+//                         break;
+//                     }
+//                 }
+
+//                 if (f) {
+//                     throw new Birthdate_Exception("Birth date Exception found!");
+//                 }
+//             } catch (Birthdate_Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             } catch (Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             }
+
+//             try {
+//                 boolean f = false;
+
+//                 for (int i = p2; i < len; i++) {
+//                     if (!(bd.charAt(i) >= '0' && bd.charAt(i) <= '9')) {
+//                         f = true;
+//                         break;
+//                     }
+//                 }
+
+//                 if (f || ((len - p2) != 4)) {
+//                     throw new Birthdate_Exception("Birth date Exception found!");
+//                 }
+//             } catch (Birthdate_Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             } catch (Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             }
+
+//             try {
+//                 if (p1 == len || p2 == len) {
+//                     throw new Birthdate_Exception("Birth date Exception found!");
+//                 }
+//             } catch (Birthdate_Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             } catch (Exception e) {
+//                 System.out.println("WrongBirthdateFormat " + e);
+//             }
+
+//         } catch (Birthdate_Exception e) {
+//             System.out.println("BirthdateBlank " + e);
+//         } catch (Exception e) {
+//             System.out.println("BirthdateBlank " + e);
+//         }
+//     }
+
+//     public static void clearScreen() {
+//         System.out.print("\033[H\033[2J");
+//         System.out.flush();
+//     }
+// }
